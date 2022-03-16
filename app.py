b69a450318fdd2df52adff54bc62ab1bd3189e8a
Wonil Kwak <one1e@naver.com>
2022-03-16 10:57:35 +0900

    update
    update 코드

--- app.py
+++ app.py
@@ -1,45 +1,23 @@
 import utils.geocoding as geo
 import pandas as pd
 
-"""
-준비물
-1. 카카오 REST API 키
-2. 주소명 컬럼이 존재하는 csv 데이터파일
+# df 설정하기
+PATH = "data/data"
+df = pd.read_csv(f"{PATH}.csv", encoding="cp949")
 
-사용방법
-1. 데이터 폴더 안에 csv 형태의 데이터를 넣어주세요
-2. 데이터파일 이름과 도로명, 지번 주소명을 가르키는 컬럼명을 지정해주세요
-3. 터미널을 열어 $ python app.py를 실행하세요.
-"""
+# 지오코딩 함수 실행
+df = geo.geocoding_action(df)
 
-# 데이터 파일 이름과 주소 컬럼명을 넣어주세요
-csv_name = "데이터 파일 이름을 넣어주세요"
-address_name = "데이터 파일에 주소명을 담은 컬럼명을 넣어주세요"
+# 지오코딩 완료된 df 저장하기
+df.to_csv(f"{PATH}(지오코딩실행).csv", encoding="cp949")
 
+# # 지오코딩에 실패한 열 삭제 - 필요시 진행
+# idx = df[df["위도"]==0].index
+# print("##############################################################################################################")
+# print("지오코딩에 실패한 행의 수 :", len(idx))
+# print("지오코딩에 실패한 행의 인덱스 :", idx)
+# print("##############################################################################################################")
+# df.drop(idx, inplace=True)
+# df.reset_index(drop=True, inplace=True)
 
-# 데이터 불러오기
-df = pd.read_csv(f"data/{csv_name}.csv", encoding="cp949")
-
-# 주소 컬럼명 지정
-addr = df[f"{address_name}"]
-
-# 함수 실행
-latitude_list, longitude_list = geo.kakao_geocoding_to_list(addr)
-
-# df에 저장
-df["위도"] = latitude_list
-df["경도"] = longitude_list
-
-# 지오코딩에 실패한 열 삭제
-idx = df[df["위도"]==0].index
-print("##############################################################################################################")
-print("지오코딩에 실패한 행의 수 :", len(idx))
-print("지오코딩에 실패한 행의 인덱스 :", idx)
-print("##############################################################################################################")
-
-# 인덱스 초기화
-df.drop(idx, inplace=True)
-df.reset_index(drop=True, inplace=True)
-
-df.to_csv(f"data/{csv_name}(위도,경도포함).csv", encoding="cp949")
-print(f"지오코딩이 성공한 주소의 위도, 경도 컬럼이 추가된 데이터 파일이 저장되었습니다. 파일 이름 : {csv_name}(위도,경도포함).csv")
+# df.to_csv(f"{PATH}(지오코딩 실패 행 삭제).csv", encoding="cp949")
--- utils/geocoding.py
+++ utils/geocoding.py
@@ -1,28 +1,31 @@
 import requests
 import json
 from tqdm import tqdm
+import time
 
 kakao_rest_api_key = input("KAKAO REST API KEY를 입력하세요 :")
 latitude_list = []
 longitude_list = []
 
-def kakao_auth(kakao_rest_api_key):
-    '''
-    카카오 REST API 키 오류인지를 확인하기 위한 함수 입니다.
-    판문점 위도값의 반올림은 항상 38이여야 합니다. 만약 그렇지 않다면 REST API 키 오류입니다.
-    '''
-    try:
-        method = "GET"
-        checkurl = 'https://dapi.kakao.com/v2/local/search/address.json?query='+"경기도 파주시 군내면 대성동길 151-13"
-        headers = {"Authorization": f"KakaoAK {kakao_rest_api_key}"}
-        check = json.loads(str(requests.request(method=method, url=checkurl, headers=headers).text))
-        match = check['documents'][0]['address']
-        lat = float(match['y'])
 
-        return round(lat)
+# REST API 검증용인데 API 횟수를 사용한다는 단점이 존재하여 수정해야함
+# def kakao_auth(kakao_rest_api_key):
+#     '''
+#     카카오 REST API 키 오류인지를 확인하기 위한 함수 입니다.
+#     판문점 위도값의 반올림은 항상 38이여야 합니다. 만약 그렇지 않다면 REST API 키 오류입니다.
+#     '''
+#     try:
+#         method = "GET"
+#         checkurl = 'https://dapi.kakao.com/v2/local/search/address.json?query='+"경기도 파주시 군내면 대성동길 151-13"
+#         headers = {"Authorization": f"KakaoAK {kakao_rest_api_key}"}
+#         check = json.loads(str(requests.request(method=method, url=checkurl, headers=headers).text))
+#         match = check['documents'][0]['address']
+#         lat = float(match['y'])
 
-    except Exception:
-        print("KAKAO REST API KEY가 잘못되었습니다.")
+#         return round(lat)
+
+#     except Exception:
+#         print("KAKAO REST API KEY가 잘못되었습니다.")
 
 
 def kakao_geocoding(address):
@@ -35,22 +38,22 @@ def kakao_geocoding(address):
     url = 'https://dapi.kakao.com/v2/local/search/address.json?query='+address
     headers = {"Authorization": f"KakaoAK {kakao_rest_api_key}"}
 
-    # 카카오 REST API 키가 정상이여야만 실행
-    if kakao_auth(kakao_rest_api_key) == 38:
-        # 메인 코드
-        try:
-            result = json.loads(str(requests.request(method=method, url=url, headers=headers).text))
-            match_first = result['documents'][0]['address']
+    # # 카카오 REST API 키가 정상이여야만 실행
+    # if kakao_auth(kakao_rest_api_key) == 38:
+    # 메인 코드
+    try:
+        result = json.loads(str(requests.request(method=method, url=url, headers=headers).text))
+        match_first = result['documents'][0]['address']
 
-            latitude = float(match_first['y'])
-            longitude = float(match_first['x'])
+        latitude = float(match_first['y'])
+        longitude = float(match_first['x'])
 
-        # 지오코딩 실패한 행의 위도와 경도값은 0으로 예외처리합니다.
-        except Exception:
-            latitude = 0
-            longitude = 0
+     # 지오코딩 실패한 행의 위도와 경도값은 0으로 예외처리합니다.
+    except Exception:
+        latitude = 0
+        longitude = 0
 
-        return latitude, longitude
+    return latitude, longitude
 
 
 def kakao_geocoding_to_list(addresslist):
@@ -67,5 +70,30 @@ def kakao_geocoding_to_list(addresslist):
     return latitude_list, longitude_list
 
 
+def geocoding_action(df):
+    '''
+    input data로 df를 받아서 위도, 경도 컬럼이 추가된 df를 return 합니다.
+    최종 실행 함수입니다.
+    '''
+
+    print(df.columns)
+    time.sleep(1)
+    address_name = input("데이터 파일에 주소명을 담고 있는 컬럼명을 넣어주세요")
+    addr = df[f"{address_name}"]
+
+    latitude_list, longitude_list = kakao_geocoding_to_list(addr)
+
+    df["latitude"] = latitude_list
+    df["longitude"] = longitude_list
+
+    idx = df[df["latitude"]==0].index
+    print("##############################################################################################################")
+    print("지오코딩에 실패한 행의 수 :", len(idx))
+    print("지오코딩에 실패한 행의 인덱스 :", idx)
+    print("##############################################################################################################")
+
+    return df
+
+
 if __name__ == "__main__":
-    print("모듈을 import 해서 실행파일에서 함수로 사용하세요.\nkakao_gecoding(address) : 주소를 입력하면 return latitude, longitude \nkakao_geocoding_list(addresslist) : 주소 리스트를 입력하면 return latitude_list, longitude_list ")
+    print("모듈을 import 해서 실행파일에서 함수로 사용하세요.")
